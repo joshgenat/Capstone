d06e4ab5f163f09322b6e6c8c51f61be420bcdfa
Joshua Genat <joshua_genat@hotmail.com>
2023-11-08 22:37:56 -0500

    establish connection and read data from firebase

--- App.js
+++ App.js
@@ -1,24 +1,50 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, SafeAreaView, Text, View, FlatList } from "react-native";
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "./src/config/firebase";
 
 export default function App() {
+  const [people, setPeople] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const usersQuery = collection(db, "users");
+    onSnapshot(usersQuery, (snapshot) => {
+      let usersList = [];
+      snapshot.docs.map((doc) => usersList.push({ ...doc.data(), id: doc.id }));
+      setPeople(usersList);
+      setLoading(false);
+    });
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <View>
+      <Text>{item.username}</Text>
+    </View>
+  );
+
+  console.log(people);
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text style={styles.text}>
         Open up App.js to start working on your app!
       </Text>
-      <Button title="Test"></Button>
-    </View>
+      <FlatList
+        data={people}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      ></FlatList>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#25292e",
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  text: {
-    color: "white",
   },
 });
---
+++ src/config/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDNpHkGU9oqppjLWpJ2oeRdTX83ApkzTPg",
+  authDomain: "iot-enabled-smart-home.firebaseapp.com",
+  databaseURL: "https://iot-enabled-smart-home-default-rtdb.firebaseio.com",
+  projectId: "iot-enabled-smart-home",
+  storageBucket: "iot-enabled-smart-home.appspot.com",
+  messagingSenderId: "463953889128",
+  appId: "1:463953889128:web:d046929251d5a771a078dd",
+  measurementId: "G-V4NM0H6Y56",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = getFirestore();
